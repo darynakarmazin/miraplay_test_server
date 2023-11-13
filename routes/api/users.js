b8d340a51be9ef0a07d2ad8c77b5f57af056fe61
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-11-13 16:28:46 +0200

    Created Router (template for feature controlers)

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const router = express.Router();
 
+router.post("/register", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.post("/login", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.post("/logout", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.get("/current", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.patch("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
